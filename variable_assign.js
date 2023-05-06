ccded176623ebb20db0c2b3c20c432184b2dda85
yogeshbhagat9053 <yogeshbhagat9053@gmail.com>
2023-05-06 22:08:30 +0530

    variableassign.js
    variableassign.js

---
+++ variable_assign.js
@@ -0,0 +1,15 @@
+ var holder;
+ console.log(holder);
+ var name;
+ console.log("Rakesh Sharma");
+ var freind;
+ console.log("Vivek Varma");
+ console.log("i love only JS");
+ console.log("FirstName:- Rakesh");
+ console.log("LastName:- Sharma");
+ console.log("Age:- 22");
+ console.log("Address:- Sangola");
+ console.log("Pincode:- 413307");
+ console.log("Gender:- Male");
+ console.log("Married Status:- No");
+ console.log("")
